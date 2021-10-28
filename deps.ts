4203c029a3e7dca98f10c8c64ebeda1f1cf966bf
brad-jones <brad-jones@users.noreply.github.com>
2021-10-28 02:02:27 +0000

    [create-pull-request] automated change

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as io from "https://deno.land/std@0.108.0/io/mod.ts#^";
-export * as testing from "https://deno.land/std@0.108.0/testing/asserts.ts#^";
+export * as io from "https://deno.land/std@0.113.0/io/mod.ts#^";
+export * as testing from "https://deno.land/std@0.113.0/testing/asserts.ts#^";
